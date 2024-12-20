94d2323b8de990ac1207e1433ff086f3bff6ee01
sudhanshu mehto <sudhanshumehto693@gmail.com>
2024-12-20 13:40:57 +0530

    first commit of DSA

---
+++ bubble_sort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void bubble(int arr[], int l)
+{
+    cout<<"bubble sort";
+}
+int main(){
+    int r;
+    cout<<"enter size of array\n";
+    cin>>r;
+    int arr[r];
+    if(r>=0 && r<20)
+    {
+        for(int i=0; i<r; ++i)
+        {
+            cin>>arr[i];
+        }
+        bubble(arr,r);
+    }
+    else
+    {
+        cout<<"not valid range\n";
+    }
+}
